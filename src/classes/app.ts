eb92d830a94b8a81ebdf01160ab10d70b82d0252
Bożka Majchrzak <zewazla@gmail.com>
2021-03-27 23:55:05 +0100

    code refactor

--- src/classes/app.ts
+++ src/classes/app.ts
@@ -13,12 +13,12 @@ import { Squirtle } from "./squirtle.js";
 import { Stonjourner } from "./stonjourner.js";
 
 let pokemons: Pokemon[] = [
-    new Pikachu(), 
+    new Pikachu(),
     new Marill(),
-    new Drapion(), 
-    new Joltik(), 
-    new Roselia(), 
-    new Sandshrew(), 
+    new Drapion(),
+    new Joltik(),
+    new Roselia(),
+    new Sandshrew(),
     new Caterpie(),
     new Morpeko(),
     new Alcremie(),
@@ -34,21 +34,22 @@ function convertToDiv(pokemon: Pokemon): HTMLElement {
 
     let namePokemon = document.createElement("h2");
     namePokemon.innerHTML = pokemon.name;
-    div.appendChild(namePokemon);
     let imgPokemon = document.createElement("img");
     imgPokemon.setAttribute("src", pokemon.getLinkToImage());
-    div.appendChild(imgPokemon);
     let attackPokemon = document.createElement("h3");
     attackPokemon.innerHTML = pokemon.attack;
-    div.appendChild(attackPokemon);
     let hpPokemon = document.createElement("h5");
     hpPokemon.innerHTML = "HP: " + pokemon.hp.toString();
-    div.appendChild(hpPokemon);
     let typePokemon = document.createElement("h4");
     typePokemon.innerHTML = "Type: " + pokemon.type;
+
+
+    div.appendChild(namePokemon);
+    div.appendChild(imgPokemon);
+    div.appendChild(attackPokemon);
+    div.appendChild(hpPokemon);
     div.appendChild(typePokemon);
 
-  
 
     pokemon.showInfoAbout();
     return div;
